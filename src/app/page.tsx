0a1399ab264d8763b78d8d6db8b4964db9af84fa
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2024-08-25 23:36:29 +0500

    PROJECT PAGE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
 import Experience from "./components/Experience";
+import Projects from "./components/Projects";
 export default function Home() {
   return (
     <>
@@ -17,6 +18,7 @@ export default function Home() {
           <About />
           <Technologies />
           <Experience />
+          <Projects />
         </div>
       </div>{" "}
     </>
