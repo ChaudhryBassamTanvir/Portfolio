45a0d5a7315085480b94a2873a9e3afabf2dfd99
ChaudhryBassamTanvir <chbassamtanvir@gmail.com>
2024-08-26 20:10:16 +0500

    animation in hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,27 +1,54 @@
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
-
+  const container = (delay: any) => ({
+    hidden: { x: -100, opacity: 0 },
+    visible: {
+      x: 0,
+      opacity: 1,
+      transition: { duration: 0.5, delay: delay },
+    },
+  });
   return (
     <div className="border-b border-neutral-900 pb-4 lg:mb-32 overflow-hidden">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 md:text-5xl text-4xl font-thin tracking-tight lg:mt-16 lg:text-6xl">
+            <motion.h1
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-16 md:text-5xl text-4xl font-thin tracking-tight lg:mt-16 lg:text-6xl"
+            >
               CHAUDHRY BASSAM TANVIR
-            </h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-transparent text-4xl tracking-tight">
+            </motion.h1>
+            <motion.span
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-transparent text-4xl tracking-tight"
+            >
               Full Stack Developer
-            </span>
-            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+            </motion.span>
+            <motion.p
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className="my-2 max-w-xl py-6 font-light tracking-tighter"
+            >
               {HERO_CONTENT}
-            </p>
+            </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-6">
           <div className="flex justify-center">
-            <img
+            <motion.img
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ delay: 1.2, duration: 1 }}
               className="w-80 rounded-2xl "
               src="/./assets/my.jpg"
               alt="profile"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Technologies from "./components/Technologies";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 export default function Home() {
   return (
     <>
@@ -19,6 +20,7 @@ export default function Home() {
           <Technologies />
           <Experience />
           <Projects />
+          <Contact />
         </div>
       </div>{" "}
     </>
